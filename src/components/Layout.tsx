6647f30fd9f0314a8014387cadfabfb117e6c963
nari <naringst@gmail.com>
2023-10-13 15:08:38 +0900

    feat : 레이아웃 - 헤더, 푸터 설정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,59 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Layout() {
   return (
     <div>
-      <header>header</header>
+      <Header>
+        <div>
+          <StyledLink to="/">SHOP</StyledLink>
+        </div>
+        <div>
+          <StyledLink to="cart">
+            <Span>장바구니</Span>
+          </StyledLink>
+          <StyledLink to="mypage">
+            <Span>마이페이지</Span>
+          </StyledLink>
+          <StyledLink to="login">
+            <Span>로그인</Span>
+          </StyledLink>
+        </div>
+      </Header>
       <Outlet />
-      <footer>footer</footer>
+      <Footer>@all rights reserved</Footer>
     </div>
   );
 }
+
+const Header = styled.header`
+  height: 30px;
+  padding: 20px 50px;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0 4px 2px -2px #d9d9d9;
+`;
+
+const Div = styled.div`
+  display: flex;
+`;
+
+const Span = styled.span`
+  margin: 0px 5px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
+
+const Footer = styled.footer`
+  padding: 10px 0;
+  width: 100%;
+  height: 50px;
+  position: fixed;
+  bottom: 0;
+  background: #d9d9d9;
+`;
