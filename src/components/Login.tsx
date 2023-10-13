318695f9f83028b20f9ee1c98d1bf286f00e5f80
nari <naringst@gmail.com>
2023-10-14 08:23:34 +0900

    feat : 로그인 페이지 UI 구현

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { ColumnFlexDiv, Container } from "../style";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <h1>로그인</h1>
+      <ColumnFlexDiv>
+        <Input></Input>
+        <Input></Input>
+      </ColumnFlexDiv>
+      <LoginButton>로그인</LoginButton>
+      <span>
+        회원이 아니신가요? <Link to="/signup">회원가입하기</Link>
+      </span>
+    </Container>
+  );
 }
+
+const Input = styled.input`
+  width: 300px;
+  height: 30px;
+  margin: 10px 0;
+`;
+
+const LoginButton = styled.button`
+  width: 310px;
+  height: 40px;
+  margin: 10px 0;
+`;
---
+++ src/style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ColumnFlexDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+`;
+
+export const Container = styled.div`
+  padding-bottom: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
