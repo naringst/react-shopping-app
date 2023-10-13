6aaab575cf6f8f7b1a7f3195d39b5c77f72c885a
nari <naringst@gmail.com>
2023-10-13 14:43:51 +0900

    feat : react-router-dom 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="cart" element={<Cart />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
