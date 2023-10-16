6a7d1028c9c9908883f0d526550e7de448a32400
nari <naringst@gmail.com>
2023-10-16 11:50:45 +0900

    feat : 장바구니 페이지 생성

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default function Cart() {
-  return <div>Cart</div>;
+export default function Cart({ cartItem, setCartItem }: any) {
+  const totalPrice = 0;
+  const deleteItem = (id: number) => {
+    setCartItem(
+      cartItem.filter((item: any) => {
+        return item.id !== id;
+      })
+    );
+  };
+  return (
+    <div>
+      {cartItem.map((item: any) => (
+        <div>
+          <div>{item?.title}</div>
+          <div>{item?.price}</div>
+
+          <div>counter</div>
+          <button>삭제</button>
+        </div>
+      ))}
+      <div>
+        <span>합계 : ${totalPrice}</span>
+        <button>계산하기</button>
+      </div>
+    </div>
+  );
 }
