08b8fbd7cb5e9c6ed6259ba5b73889ef6167b860
nari <naringst@gmail.com>
2023-10-16 11:50:10 +0900

    feat : 장바구니 상태 추가 및 props 전달

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,33 @@ import Cart from "./components/Cart";
 import Login from "./components/Login";
 import Layout from "./components/Layout";
 import Mypage from "./components/Mypage";
+import Detail from "./components/Detail";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Item } from "./interface/interface";
 
 function App() {
+  const [cartItem, setCartItem] = useState<Item[]>([]);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />}></Route>
-          <Route path="cart" element={<Cart />}></Route>
+          <Route
+            path="/detail/:id"
+            element={<Detail cartItem={cartItem} setCartItem={setCartItem} />}
+          ></Route>
+          <Route
+            path="cart"
+            element={<Cart cartItem={cartItem} setCartItem={setCartItem} />}
+          ></Route>
           <Route path="mypage" element={<Mypage />}></Route>
           <Route path="login" element={<Login />}></Route>
         </Route>
       </Routes>
+      Detail
     </div>
   );
 }
