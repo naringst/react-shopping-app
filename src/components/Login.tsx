84b47907ede81bbb378ff54f5f1c3986f4942059
Ellie <naringst@gmail.com>
2023-10-17 10:25:51 +0900

    feat: 로그인, 회원가입 기본 설정

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { ColumnFlexDiv, Container } from "../style";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    console.log("로그인 인증");
+  };
   return (
     <Container>
       <h1>로그인</h1>
       <ColumnFlexDiv>
-        <Input type="email"></Input>
-        <Input type="password"></Input>
+        <Input type="email" value={email}></Input>
+        <Input type="password" value={pw}></Input>
       </ColumnFlexDiv>
-      <LoginButton>로그인</LoginButton>
+      <LoginButton onSubmit={handleLogin}>로그인</LoginButton>
       <span>
         회원이 아니신가요? <Link to="/signup">회원가입하기</Link>
       </span>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Container } from "../style";
 
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [pwCheck, setPwCheck] = useState("");
+
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handlePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPw(e.target.value);
+  };
+  const handlePwCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPwCheck(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("submit");
+    console.log(email);
+    console.log(pw);
+    console.log(pwCheck);
+    if (pw === pwCheck) {
+    }
+  };
+
   return (
     <Container>
-      <label htmlFor="email">아이디</label>{" "}
-      <Input id="email" type="email"></Input>
-      <label htmlFor="pw">비밀번호</label> <Input id="pw" type="pw"></Input>
-      <label htmlFor="pw">비밀번호 확인</label>
-      <Input id="pw" type="pw"></Input>
-      <LoginButton>회원가입</LoginButton>
+      <form>
+        <label htmlFor="email">아이디</label>
+        <Input
+          id="sign-up-email"
+          type="email"
+          value={email}
+          onChange={handleEmail}
+        />
+        <label htmlFor="sign-up-pw">비밀번호</label>
+        <Input id="sign-up-pw" type="password" value={pw} onChange={handlePw} />
+        <label htmlFor="sign-up-pw">비밀번호 확인</label>
+        <Input
+          id="pw"
+          type="password"
+          value={pwCheck}
+          onChange={handlePwCheck}
+        ></Input>
+        <LoginButton onSubmit={handleSubmit}>회원가입</LoginButton>
+      </form>
     </Container>
   );
 }
