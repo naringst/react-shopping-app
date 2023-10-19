fe65282512fa2dad71ce3b9b8559f65bc1ec12db
nari <naringst@gmail.com>
2023-10-19 14:24:28 +0900

    feat : firebase활용 로그인 기능 구현

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,33 +1,70 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Container } from "../style";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
-  const [pw, setPw] = useState("");
+  const [password, setPassword] = useState("");
   const [pwCheck, setPwCheck] = useState("");
+  // Import the functions you need from the SDKs you need
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDk8anwaHkB3mD5wXJ1oW0cuc65zDOKzZQ",
+    authDomain: "react-shopping-7c569.firebaseapp.com",
+    projectId: "react-shopping-7c569",
+    storageBucket: "react-shopping-7c569.appspot.com",
+    messagingSenderId: "1046827400753",
+    appId: "1:1046827400753:web:25e1cc9d4539884b009f10",
+    measurementId: "G-59ZPS6CMCW",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
     setEmail(e.target.value);
   };
 
   const handlePw = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setPw(e.target.value);
+    setPassword(e.target.value);
   };
   const handlePwCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
     setPwCheck(e.target.value);
   };
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const handleSubmit = async (e: any) => {
+    //비밀번호 6자 이상이어야 ...
     console.log("submit");
     console.log(email);
-    console.log(pw);
+    console.log(password);
     console.log(pwCheck);
-    if (pw === pwCheck) {
+    if (password === pwCheck) {
+      const auth = getAuth();
+
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          console.log(user);
+          setEmail("");
+          setPassword("");
+          setPwCheck("");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
     }
   };
 
@@ -35,23 +72,23 @@ export default function SignUp() {
     <Container>
       <form>
         <label htmlFor="email">아이디</label>
+        <Input name="email" type="email" value={email} onChange={handleEmail} />
+        <label htmlFor="password">비밀번호</label>
         <Input
-          id="sign-up-email"
-          type="email"
-          value={email}
-          onChange={handleEmail}
+          name="password"
+          type="password"
+          value={password}
+          onChange={handlePw}
         />
-        <label htmlFor="sign-up-pw">비밀번호</label>
-        <Input id="sign-up-pw" type="password" value={pw} onChange={handlePw} />
-        <label htmlFor="sign-up-pw">비밀번호 확인</label>
+        <label htmlFor="passwordCheck">비밀번호 확인</label>
         <Input
-          id="pw"
+          name="passwordCheck"
           type="password"
           value={pwCheck}
           onChange={handlePwCheck}
         ></Input>
-        <LoginButton onSubmit={handleSubmit}>회원가입</LoginButton>
       </form>
+      <LoginButton onClick={handleSubmit}>회원가입</LoginButton>
     </Container>
   );
 }
